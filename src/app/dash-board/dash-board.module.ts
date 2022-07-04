e652bf61f31c64db1820720887c673c6095bfdcb
meghana <meghana.urlana@gmail.com>
2022-07-04 17:00:59 +0530

    modules

--- src/app/dash-board/dash-board.module.ts
+++ src/app/dash-board/dash-board.module.ts
@@ -2,6 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToastrModule } from 'ngx-toastr';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 
 
@@ -11,7 +21,18 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    InputTextModule,
+    RadioButtonModule,
+    ButtonModule,
+    FormsModule, ReactiveFormsModule,
+    BrowserAnimationsModule,
+    PasswordModule,
+    SidebarModule,
+    ToastModule,
+    ToastrModule,
+    
+
   ]
 })
 export class DashBoardModule { }
--- src/app/profile-users/profile-users.module.ts
+++ src/app/profile-users/profile-users.module.ts
@@ -3,6 +3,16 @@ import { CommonModule } from '@angular/common';
 import { ProfileComponent } from './profile/profile.component';
 import { UsersComponent } from './users/users.component';
 import { RouterModule } from '@angular/router';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { ButtonModule } from 'primeng/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PasswordModule } from 'primeng/password';
+import { SidebarModule } from 'primeng/sidebar';
+import { ToastrModule } from 'ngx-toastr';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 
 
@@ -13,7 +23,17 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    RouterModule,
+    InputTextModule,
+    RadioButtonModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    FormsModule, ReactiveFormsModule,
+    PasswordModule,
+    SidebarModule,
+    ToastrModule,
+    ToastModule
   ]
 })
 export class ProfileUsersModule { }
